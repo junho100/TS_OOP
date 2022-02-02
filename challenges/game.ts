3e26e3ade47b10ffb80ea4d66102fcfd7f4e2669
JunhoBaek <bemodesty306@naver.com>
2022-02-02 16:10:06 +0900

    type challenge 완료

--- challenges/game.ts
+++ challenges/game.ts
@@ -1,12 +1,45 @@
 /**
  * Let's make a game 🕹
  */
+type Position = {
+  x: number;
+  y: number;
+};
+type Move = "up" | "down" | "left" | "right";
+let position: Position = {
+  x: 0,
+  y: 0,
+};
+const move = (op: Move): void => {
+  switch (op) {
+    case "up": {
+      position.y++;
+      break;
+    }
+    case "down": {
+      position.y--;
+      break;
+    }
+    case "left": {
+      position.x--;
+      break;
+    }
+    case "right": {
+      position.x++;
+      break;
+    }
+    default: {
+      throw new Error("error!");
+    }
+  }
+};
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
--- challenges/loading.ts
+++ challenges/loading.ts
@@ -2,25 +2,46 @@
   /**
    * Print Loading State
    */
+
   type LoadingState = {
-    state: 'loading';
+    state: "loading";
   };
 
   type SuccessState = {
-    state: 'success';
+    state: "success";
     response: {
       body: string;
     };
   };
 
   type FailState = {
-    state: 'fail';
+    state: "fail";
     reason: string;
   };
 
   type ResourceLoadState = LoadingState | SuccessState | FailState;
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  const printLoginState = (result: ResourceLoadState) => {
+    switch (result.state) {
+      case "loading": {
+        console.log("👀 loading...");
+        break;
+      }
+      case "success": {
+        console.log(`😃 ${result.response.body}`);
+        break;
+      }
+      case "fail": {
+        console.log(`😱 ${result.reason}`);
+        break;
+      }
+      default: {
+        throw new Error("error!");
+      }
+    }
+  };
+
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
 }
