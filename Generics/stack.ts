bb87269f75e63f9b8a4e590524140ac59346ae9f
JunhoBaek <bemodesty306@naver.com>
2022-02-04 17:06:27 +0900

    generic type 수정(완료)

--- Generics/stack.ts
+++ Generics/stack.ts
@@ -1,27 +1,28 @@
 {
-  interface IStack {
+  interface IStack<T> {
     readonly size: number;
-    push(value: number): void;
-    pop(): number;
+    push(value: T): void;
+    pop(): T;
   }
 
-  interface IStackNode {
-    value: number;
-    prevNode: IStackNode | undefined;
+  interface IStackNode<T> {
+    value: T;
+    //타입 통일?
+    prevNode: IStackNode<T> | undefined;
   }
 
-  class Stack implements IStack {
+  class Stack<T> implements IStack<T> {
     private _size: number;
     get size() {
       return this._size;
     }
-    presentNode: IStackNode | undefined;
+    presentNode: IStackNode<T> | undefined;
     constructor() {
       this._size = 0;
       this.presentNode = undefined;
     }
 
-    push = (value: number): void => {
+    push = (value: T): void => {
       this._size += 1;
       if (this.presentNode == null) {
         this.presentNode = new StackNode(value, undefined);
@@ -33,7 +34,7 @@
       return;
     };
 
-    pop = (): number => {
+    pop = (): T => {
       if (this.presentNode == null) {
         throw new Error("Empty Stack!");
       }
@@ -46,11 +47,11 @@
     };
   }
 
-  class StackNode implements IStackNode {
-    value: number;
-    prevNode: IStackNode | undefined;
+  class StackNode<T> implements IStackNode<T> {
+    value: T;
+    prevNode: IStackNode<T> | undefined;
 
-    constructor(value: number, prevNode: IStackNode | undefined) {
+    constructor(value: T, prevNode: IStackNode<T> | undefined) {
       this.value = value;
       this.prevNode = prevNode;
     }
@@ -59,8 +60,9 @@
   const S = new Stack();
 
   S.push(1);
-  S.push(3);
-  S.push(2);
+  S.push("a");
+  S.push({ a: "hi" });
+  S.pop();
   S.pop();
   S.pop();
 }
