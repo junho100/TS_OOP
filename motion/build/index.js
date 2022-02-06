773f833d685b058a82348bf2faaab4208eb2ae2c
JunhoBaek <bemodesty306@naver.com>
2022-02-06 13:47:39 +0900

    모달 창 구현

--- motion/build/index.js
+++ motion/build/index.js
@@ -1,2 +1,46 @@
 "use strict";
-console.log("test");
+var makeAppDark = function () {
+    var app = document.getElementById("app");
+    app.style.filter = "brightness(30%)";
+};
+var makeAppBright = function () {
+    var app = document.getElementById("app");
+    app.style.filter = "none";
+};
+var renderModal = function (id) {
+    var modals = document.getElementsByClassName("modal");
+    for (var i = 0; i < 4; i++) {
+        if (modals[i].id === "".concat(id, "_modal")) {
+            var target = document.getElementById("".concat(id, "_modal"));
+            target.style.display = "block";
+        }
+        else {
+            var target = document.getElementById(modals[i].id);
+            target.style.display = "none";
+        }
+    }
+};
+var closeModal = function () {
+    var modals = document.getElementsByClassName("modal");
+    for (var j = 0; j < 4; j++) {
+        modals[j].style.display = "none";
+    }
+};
+var menu = document.getElementsByClassName("menu_btn");
+var cls = document.getElementsByClassName("close");
+var add = document.getElementsByClassName("add");
+for (var i = 0; i < 4; i++) {
+    menu[i].addEventListener("click", function (e) {
+        makeAppDark();
+        var id = e.target.id;
+        renderModal(id);
+    });
+    cls[i].addEventListener("click", function (e) {
+        e.preventDefault();
+        makeAppBright();
+        closeModal();
+    });
+    add[i].addEventListener("click", function (e) {
+        e.preventDefault();
+    });
+}
--- motion/src/index.ts
+++ motion/src/index.ts
@@ -1,1 +1,52 @@
-console.log("test");
+const makeAppDark = (): void => {
+  const app: HTMLElement | null = document.getElementById("app");
+  (app as HTMLElement).style.filter = "brightness(30%)";
+};
+const makeAppBright = (): void => {
+  const app: HTMLElement | null = document.getElementById("app");
+  (app as HTMLElement).style.filter = "none";
+};
+const renderModal = (id: string): void => {
+  const modals = document.getElementsByClassName("modal");
+  for (let i = 0; i < 4; i++) {
+    if (modals[i].id === `${id}_modal`) {
+      const target: HTMLElement = document.getElementById(
+        `${id}_modal`
+      ) as HTMLElement;
+      target.style.display = "block";
+    } else {
+      const target: HTMLElement | null = document.getElementById(modals[i].id);
+      (target as HTMLElement).style.display = "none";
+    }
+  }
+};
+const closeModal = (): void => {
+  const modals: HTMLCollectionOf<Element> =
+    document.getElementsByClassName("modal");
+  for (let j = 0; j < 4; j++) {
+    (modals[j] as HTMLElement).style.display = "none";
+  }
+};
+
+const menu: HTMLCollectionOf<Element> =
+  document.getElementsByClassName("menu_btn");
+const cls: HTMLCollectionOf<Element> = document.getElementsByClassName("close");
+const add: HTMLCollectionOf<Element> = document.getElementsByClassName("add");
+
+for (let i = 0; i < 4; i++) {
+  menu[i].addEventListener("click", (e: Event) => {
+    makeAppDark();
+    const id: string = (e.target as Element).id;
+    renderModal(id);
+  });
+
+  cls[i].addEventListener("click", (e: Event) => {
+    e.preventDefault();
+    makeAppBright();
+    closeModal();
+  });
+
+  add[i].addEventListener("click", (e: Event) => {
+    e.preventDefault();
+  });
+}
